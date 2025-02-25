b9223c5037ea6a5b7ee2c4822f43dc73488730d7
Shivam Patel <shivampatel0048@gmail.com>
2025-02-25 21:30:48 +0530

    Add two approaches for hollow diamond pattern in C++

--- Chapter 4 Arrays/patterns/hollow-diamond-pattern.cpp
+++ Chapter 4 Arrays/patterns/hollow-diamond-pattern.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 using namespace std;
 
-int main()
+// 1st Approach
+void approach1(int n)
 {
-    int n = 4;
-
     // Upper half of diamond
     for (int i = 0; i < n; i++)
     {
@@ -42,6 +41,59 @@ int main()
         }
         cout << endl;
     }
+}
+
+// 2nd Approach
+void approach2(int n)
+{
+    // Upper half of diamond
+    for (int i = 1; i <= n; i++)
+    {
+        // Print spaces
+        for (int j = i; j < n; j++)
+        {
+            cout << "  ";
+        }
+        // Print hollow pattern
+        for (int j = 1; j <= (2 * i - 1); j++)
+        {
+            if (j == 1 || j == (2 * i - 1))
+                cout << " * ";
+            else
+                cout << "  ";
+        }
+        cout << endl;
+    }
+
+    // Lower half of diamond
+    for (int i = n - 1; i >= 1; i--)
+    {
+        // Print spaces
+        for (int j = n; j > i; j--)
+        {
+            cout << "  ";
+        }
+        // Print hollow pattern
+        for (int j = 1; j <= (2 * i - 1); j++)
+        {
+            if (j == 1 || j == (2 * i - 1))
+                cout << " * ";
+            else
+                cout << "  ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int n = 4;
+
+    cout << "1st Approach:" << endl;
+    approach1(n);
+
+    cout << "2nd Approach:" << endl;
+    approach2(n);
 
     return 0;
 }
