bcdf58d5085dbdf81f9fe659b00df5f7fe811d4a
Shivam Patel <shivampatel0048@gmail.com>
2025-02-26 18:41:58 +0530

    Add multiple approaches for inverted triangle pattern in C++

--- Chapter 4 Arrays/patterns/inverted-triangle-pattern.cpp
+++ Chapter 4 Arrays/patterns/inverted-triangle-pattern.cpp
@@ -1,28 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int main()
+// 1st Approach: Using characters
+void approach1(int n, char c)
 {
-    int n = 4;
-    char c = 'A';
-
-    // for (int i = 0; i < n; i++)
-    // {
-    //     c= c+i;
-    //     for (int k = 0; k < i; k++)
-    //     {
-    //         cout << "  ";
-    //     }
-    //     for (int j = n; j > i; j--)
-    //     {
-    //         cout << n - i<< " ";
-    //     }
-    //     cout << endl;
-    // }
-
     for (int i = 0; i < n; i++)
     {
-
         for (int k = 0; k < i; k++)
         {
             cout << "  ";
@@ -33,6 +16,55 @@ int main()
         }
         cout << endl;
     }
+}
+
+// 2nd Approach: Using numbers
+void approach2(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int k = 0; k < i; k++)
+        {
+            cout << "  ";
+        }
+        for (int j = n; j > i; j--)
+        {
+            cout << n - i << " ";
+        }
+        cout << endl;
+    }
+}
+
+// 3rd Approach: Using stars
+void approach3(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int k = 0; k < i; k++)
+        {
+            cout << "  ";
+        }
+        for (int j = n; j > i; j--)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int n = 4;
+    char c = 'A';
+
+    cout << "1st Approach: Using characters" << endl;
+    approach1(n, c);
+
+    cout << "2nd Approach: Using numbers" << endl;
+    approach2(n);
+
+    cout << "3rd Approach: Using stars" << endl;
+    approach3(n);
 
     return 0;
 }
